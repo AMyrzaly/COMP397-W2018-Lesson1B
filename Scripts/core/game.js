0755eac58fac909b8e97e6d4f29f88a2025df410
Imba-lol <best.abuka@gmail.com>
2018-02-04 21:07:13 -0500

    initial commit

---
+++ Scripts/core/game.js
@@ -0,0 +1,25 @@
+// IIFE - Immediately Invoked Function Expression
+(function(){
+
+    function Init(){
+        console.log("Initialization Started");
+
+        let myVariable = "foo";
+        let myVariable = 1;
+    }
+
+    function Start(){
+
+    }
+
+    function Update(){
+
+    }
+
+    function Main(){
+
+    }
+
+    window.onload = Init;
+
+})();
